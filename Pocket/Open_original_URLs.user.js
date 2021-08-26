27df37fa7d945ab457b3e0db94256db696cb81d9
Jorengarenar <jorengarenar@outlook.com>
2021-08-26 21:14:13 +0200

    Cleaning

--- Pocket/Open_original_URLs.user.js
+++ Pocket/Open_original_URLs.user.js
@@ -3,33 +3,20 @@
 // @description  Opens original links instead of article view
 // @author       Jorengarenar
 // @namespace    https://joren.ga
-// @version      0.5.1
+// @version      0.6.0
 // @match        https://getpocket.com/*
 // @run-at       document-end
 // ==/UserScript==
 
-
-const config = {
-  childList: true,
-  subtree: true
-};
-
-function change(queue, observer) {
-  window.setTimeout(function() {
-    let articlesList = document.querySelectorAll("article");
-    for (let article of articlesList) {
-      let originalURL = article.querySelector("a.publisher");
-      let link = article.querySelector(".content > h2.title");
-      if (originalURL && link.querySelector("a[tabindex=\"0\"]")) {
-        let newURL = document.createElement("a");
-        newURL.innerText = link.querySelector("a").innerText;
-        newURL.href = originalURL.href;
-        link.replaceChild(newURL, link.childNodes[0]);
-      }
+new MutationObserver(function() {
+  document.querySelectorAll("article").forEach((article) => {
+    let originalURL = article.querySelector("a.publisher");
+    let link = article.querySelector(".content > h2.title");
+    if (originalURL && link.querySelector("a[tabindex=\"0\"]")) {
+      let newURL = document.createElement("a");
+      newURL.textContent = link.querySelector("a").textContent;
+      newURL.href = originalURL.href;
+      link.replaceChild(newURL, link.childNodes[0]);
     }
-  }, 1000);
-}
-
-const observer = new MutationObserver(change);
-change(document.body, config);
-observer.observe(document.body, config);
+  });
+}).observe(document.body, { childList: true, subtree: true });
