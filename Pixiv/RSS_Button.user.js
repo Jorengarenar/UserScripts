94114c4f7e367fe67d9a0d998b4653bc5b4a49cc
Jorengarenar <jorengarenar@outlook.com>
2022-05-08 06:02:24 +0200

    Update

--- Pixiv/RSS_Button.user.js
+++ Pixiv/RSS_Button.user.js
@@ -3,20 +3,28 @@
 // @description  RSS using RSSHub.app
 // @author       Jorengarenar
 // @namespace    https://joren.ga
-// @version      1.0.0
+// @version      1.5.0
 // @run-at       document-end
-// @include      /https?:\/\/www\.pixiv\.net\/.*users\/\d+/
+// @match        https://www.pixiv.net/*
 // ==/UserScript==
 
-let rssButton = document.createElement("a");
+const rssButton = document.createElement('a');
 rssButton.target = "_blank";
 rssButton.innerHTML = "<b>RSS</b>";
-rssButton.setAttribute("class", "_2Of8xxg");
-let id = window.location.href.match(/users\/(\d+)/)[1];
-rssButton.href = "https://rsshub.app/pixiv/user/" + id;
+rssButton.setAttribute('class', "_2Of8xxg");
 
-window.setTimeout(function() {
-  Array.from(document.getElementsByTagName("button"))
-    .find((el) => el.textContent.match(/Follow(ing)?/))
-    .parentNode.prepend(rssButton);
-}, 100);
+window.onload = function() {
+  new MutationObserver(function() {
+    if (! /users\/\d+/.test(window.location.pathname)) { return; }
+
+    const id = window.location.href.match(/users\/(\d+)/)[1];
+    rssButton.href = "https://rsshub.app/pixiv/user/" + id;
+
+    const bar = Array.from(document.querySelectorAll('button:first-child'))
+      .find(el => el.textContent.match(/Follow(ing)?/))?.parentNode;
+
+    if (!bar || bar.childNodes.length < 3) { return; }
+
+    bar.prepend(rssButton);
+  }).observe( document.body, { childList: true, subtree: true } );
+};
